abd386cc4d1aec655caea1cfed59ce8a335402f7
adm1n123 <deepakbaghel31@gmail.com>
2021-03-02 18:14:01 +0530

    testing

---
+++ client.c
@@ -0,0 +1,84 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+
+void prepare_query(char *buff, int buff_len, long low, long high) {
+	sleep(1);
+	if(high <= low) high = low + 1;
+	long num = low + rand() % (high - low);
+	int i = buff_len - 1;
+	while(num > 0) {
+		buff[i--] = num % 10 + '0';
+		num /= 10;
+	}
+	while(i >= 0) buff[i--] = '\0';
+	strncpy(buff, "prime", 5);
+}
+
+void print(char *buff, int len) {
+	for(int i = 0; i < len; i++) printf("%c", buff[i]);
+}
+void println(char *buff, int buff_len) {
+	print(buff, buff_len);
+	printf("\n");
+}
+
+void query(int server_fd) {
+	
+	static int buff_len = 25;
+	char buff[25];
+	printf("Query: ");
+	while(1) {
+		bzero(buff, sizeof(buff));
+		strncpy(buff, "prime", 5);
+		prepare_query(buff, buff_len, 0, 50);
+		println(buff, buff_len);
+
+		write(server_fd, buff, sizeof(buff));
+		if(strncmp(buff, "exit", 4) == 0) break;
+
+		bzero(buff, sizeof(buff));
+		read(server_fd, buff, sizeof(buff));
+		printf("Server: ");
+		println(buff, buff_len);
+		printf("Query: ");
+	}
+	printf("Client exit\n");
+}
+
+// create load balancer emthod and create thread for each server fd and call query method for each server fd. and also when server stops make that thread break the query method and close server fd.
+// when server starts(monitor notifies) then create new thread(i.e call query method for thread).
+// main thread will communicate with monitor.
+
+void main() {
+	struct sockaddr_in server_address;
+
+	int sock_fd, flag;
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock_fd == -1) {
+		printf("socket creation failed");
+		exit(0);
+	} else printf("socket created\n");
+
+	server_address.sin_family = AF_INET;
+	// IP of server pc to connect with. INADDR_LOOPBACK is 127.0.0.1 i.e. localhost you can specify IP
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY); //inet_addr("192.168.122.89");
+	// port of server process on server pc.
+	server_address.sin_port = htons(8080);
+
+	flag = connect(sock_fd, (struct sockaddr *)&server_address, sizeof(server_address));
+	if(flag == -1) {
+		printf("Error conecting\n");
+		exit(0);
+	} else printf("connected\n");
+
+	query(sock_fd);
+	close(sock_fd);
+}
---
+++ server.h
@@ -0,0 +1,55 @@
+
+
+bool is_prime(long n);
+void sum_prime(char *buff, int n);
+
+void print(char *buff, int len) {
+	for(int i = 0; i < len; i++) printf("%c", buff[i]);
+}
+void println(char *buff, int buff_len) {
+	print(buff, buff_len);
+	printf("\n");
+}
+
+
+bool is_prime(long n) {
+	if(n < 2) return false;
+	for(int i = 2; i < n; i++) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+void sum_prime(char *buff, int len) {
+	if(strncmp(buff, "prime", 5) != 0) {
+		printf("Error processing request\n");
+		strncpy(buff, "Error", 5);
+		return;
+	}
+
+	long num = 0;
+	for(int i = 5; i < len; i++) {
+		if(buff[i] != '\0') {
+			num *= 10;
+			num += buff[i] - '0';
+			printf("%d, ", buff[i]-'0');
+		}
+	}
+
+	// printf("\nFound num query:%ld, ", num);
+	long sum = 0;
+	for(int i = 2; i <= num; i++) {
+		if(is_prime(i) == true) {
+			sum += i;
+			// printf("prime: %d, ", i);
+		}
+	}
+	// printf("\n");
+	int i = len-1;
+	while(sum > 0) {
+		buff[i--] = sum % 10 + '0';
+		sum /= 10;
+	}
+	while(i >= 0) buff[i--] = '\0';
+	strncpy(buff, "sum", 3);
+}
