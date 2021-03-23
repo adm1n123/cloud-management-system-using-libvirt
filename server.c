044d2caf5996e0edf12bef17e639b349131515e2
adm1n123 <deepakbaghel31@gmail.com>
2021-03-24 00:47:56 +0530

    serverlogs

--- server.c
+++ server.c
@@ -127,7 +127,7 @@ int create_lstn_sock_fd() {
 }
 
 void init_logs() {
-	sleep(10);
+	sleep(30);
 	logs_fd = fopen("server.logs", "w");
 	if(logs_fd == NULL) {
 		fprintf(stderr, "Server logs creation failed\n");
